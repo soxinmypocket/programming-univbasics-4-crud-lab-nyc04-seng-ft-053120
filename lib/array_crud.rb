a4000f8afd4b9d0e8c566411522b3d42daac2742
Shawnae J <shawnaejohnson6@gmail.com>
2020-06-09 01:35:38 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -3,7 +3,7 @@ def create_an_empty_array
 end
 
 def create_an_array
-  create_an_array = ["hi", "hello", "bye", "arrays!"]
+  create_an_array = ["wow", "hello", "bye", "arrays!"]
 end
 
 def add_element_to_end_of_array(array, element)
